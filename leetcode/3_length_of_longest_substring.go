af8eea785f7ba7921d83a8338ef3fe575dbb69b3
Nikita Shmorin <shmorin.n@citilink.ru>
2023-12-11 14:31:46 +0300

    рефакторинг leetcode задачи 3

--- leetcode/3_length_of_longest_substring.go
+++ leetcode/3_length_of_longest_substring.go
@@ -2,30 +2,21 @@ package leetcode
 
 func lengthOfLongestSubstring2(s string) int {
 	runes := []rune(s)
+	left, res := 0, 0
 
-	if len(runes) == 1 {
-		return 1
-	}
-
-	state := make(map[rune]int)
-
-	res, left := 0, 0
+	state := make(map[rune]struct{})
 
 	for right := 0; right < len(runes); right++ {
 		if _, ok := state[runes[right]]; ok {
-			state[runes[left]]--
-			if state[runes[left]] == 0 {
-				delete(state, runes[left])
-			}
-
+			delete(state, runes[left])
 			left++
 			right--
 			continue
 		} else {
-			state[runes[right]]++
+			state[runes[right]] = struct{}{}
 		}
 
-		res = max(res, right-left+1)
+		res = max(res, len(state))
 	}
 
 	return res
