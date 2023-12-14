38bac0b286f06a8b10342b75e5df897b3eb7d5fc
Nikita Shmorin <shmorin.n@citilink.ru>
2023-12-14 15:36:04 +0300

    решена leetcode задача 424

---
+++ leetcode/424_longest_repeating_character_replacement.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func characterReplacement(s string, k int) int {
+	left, res, maxFrequently := 0, 0, 0
+	m := make(map[byte]int)
+
+	for right := 0; right < len(s); right++ {
+		m[s[right]]++
+		maxFrequently = max(maxFrequently, m[s[right]])
+
+		if right-left+1-maxFrequently > k {
+			m[s[left]]--
+			left++
+		}
+
+		res = max(res, right-left+1)
+	}
+
+	return res
+}
