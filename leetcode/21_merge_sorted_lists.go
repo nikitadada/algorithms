8829bfc5c25a0c30e09c569f74d1cb1b4fd01754
Nikita Shmorin <shmorin.n@citilink.ru>
2023-12-26 12:55:21 +0300

    решена проще leetcode задача 21

--- leetcode/21_merge_sorted_lists.go
+++ leetcode/21_merge_sorted_lists.go
@@ -31,3 +31,26 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 
 	return dummyNode.Next
 }
+
+// Решение без использования +infinity, но с более сложным условием в цикле.
+//func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+//	dummyNode := &ListNode{}
+//
+//	p1 := list1
+//	p2 := list2
+//
+//	curr := dummyNode
+//	for p1 != nil || p2 != nil {
+//		if p1 != nil && (p2 == nil || p1.Val < p2.Val) {
+//			curr.Next = p1
+//			p1 = p1.Next
+//		} else {
+//			curr.Next = p2
+//			p2 = p2.Next
+//		}
+//
+//		curr = curr.Next
+//	}
+//
+//	return dummyNode.Next
+//}
